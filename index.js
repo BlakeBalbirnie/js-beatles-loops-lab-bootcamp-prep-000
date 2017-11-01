a903ead156b2db329752fd7c115271a9573dd812
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-01 18:54:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function theBeatlesPlay(players, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "foo", "bar"];
   var exclamationFacts = [];
-  while ( facts != '') {
+  while (facts != '') {
     exclamationFacts.push(facts[0] +"!!!");
     facts.shift();
   }
