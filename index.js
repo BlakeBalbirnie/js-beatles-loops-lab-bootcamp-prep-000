909e44e3e9416ba130d9a22e3c33fd0ae2f4eb7d
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-01 18:43:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function theBeatlesPlay(players, instruments) {
+  var allPlayers = [];
+  for (var i=0; i < players.length; i++) {
+    allPlayers.push(players[i] + " plays " + instruments[i]);
+  }
+  return allPlayers;
+}
